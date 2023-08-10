f0efae4b04e4be43836e89c0409a38a549bb8fdb
lemon <lemon.juice9341@gmail.com>
2023-08-11 00:56:51 +0300

    This class displays a chessboard

---
+++ src/com/tms/lesson5/Exercise_1.java
@@ -0,0 +1,46 @@
+package com.tms.lesson5;
+
+import java.util.Arrays;
+
+/**
+ * Шахматная доска
+ * Создать программу для раскраски шахматной доски с помощью цикла.
+ * Создать двумерный массив String'ов 8х8. С помощью циклов задать
+ * элементам циклам значения B(Black) или W(White). Результат работы
+ * программы:
+ * W B W B W B W B
+ * B W B W B W B W
+ * W B W B W B W B
+ * B W B W B W B W
+ * W B W B W B W B
+ * B W B W B W B W
+ * W B W B W B W B
+ * B W B W B W B W
+ */
+
+public class Exercise_1 {
+    public static void main(String[] args) {
+
+        int[][] array = new int[8][8];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println();
+            for (int j = 0; j < array[i].length; j++) {
+                if (i % 2 != 0) {
+                    if (j % 2 != 0) {
+                        System.out.print("W ");
+                    } else {
+                        System.out.print("B ");
+                    }
+                } else {
+                    if (j % 2 != 0) {
+                        System.out.print("B ");
+                    } else {
+                        System.out.print("W ");
+                    }
+                }
+            }
+        }
+
+    }
+}
